3f2bc817d48518d5bfd4a064a46268acebc1f94a
Danil <danilunrandom@gmail.com>
2020-03-13 18:07:31 +0300

    implemented create_user and read_user

--- src/papertext_auth/auth.py
+++ src/papertext_auth/auth.py
@@ -1,6 +1,6 @@
-from typing import Any, Callable, Dict, List, Mapping, NoReturn, Tuple
+from typing import Any, Callable, Dict, List, Mapping, NoReturn, Tuple, Union
 
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, status
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import (
     Column,
@@ -12,7 +12,7 @@ from sqlalchemy import (
     create_engine,
 )
 
-from paperback import BaseAuth, NewUser
+from paperback import BaseAuth, NewUser, UserInfo
 
 from .crypto import crypt_context
 
@@ -45,6 +45,7 @@ class AuthImplemented(BaseAuth):
             Column("id", Integer, primary_key=True),
             Column("username", String(256)),
             Column("hashed_password", String),
+            Column("full_name", String(256)),
             Column("access_level", Integer),
             Column("organization", String(256)),
         )
@@ -57,28 +58,50 @@ class AuthImplemented(BaseAuth):
             Column("location", String),
             Column("device", String),
         )
+
+        self.invitation_codes = Table(
+            "invitation_codes",
+            self.metadata,
+            Column("id", Integer, primary_key=True),
+            Column("code", String),
+            Column("issued_by", Integer),
+        )
         self.metadata.create_all(self.engine)
 
     async def create_user(
         self,
         username: str,
         password: str,
+        full_name: str = None,
         access_level: int = 0,
         organization: str = "Public",
-    ):
-        ins = self.users.insert().values(
+    ) -> Tuple[int, str]:
+        if full_name is None:
+            full_name = username
+        select = self.users.select().where(self.users.c.username == username)
+        insert = self.users.insert().values(
             username=username,
             hashed_password=crypt_context.hash(password),
+            full_name=full_name,
             access_level=access_level,
             organization=organization,
         )
-        print(ins)
-        conn = self.engine.connect()
-        res = conn.execute(ins)
-        print(res)
 
-    def read_user(self, email: str) -> Dict[str, Tuple[str, int]]:
-        pass
+        conn = self.engine.connect()
+        if len(conn.execute(select).fetchall()) > 0:
+            return status.HTTP_400_BAD_REQUEST, {"detail": f"User with username `{username}` already exists"}
+        else:
+            conn.execute(insert)
+            return status.HTTP_200_OK, {"detail": "Successfully created new user"}
+
+    async def read_user(self, username: str) -> Tuple[int, str, UserInfo]:
+        select = self.users.select().where(self.users.c.username == username)
+        conn = self.engine.connect()
+        result = conn.execute(select).fetchone()
+        print(result)
+        user = UserInfo(username=result.username, full_name=result.full_name, organization=result.organization, access_level=result.access_level)
+        return status.HTTP_200_OK, {"detail": f"Successfully found user {result.id} by username"}, user
+
 
     def update_user(
         self,
@@ -93,13 +116,13 @@ class AuthImplemented(BaseAuth):
     def delete_user(self, email: str) -> bool:
         pass
 
-    def sign_in(self, email: str, password: str,) -> str:
+    def sign_in(self, username: str, password: str, ) -> str:
         pass
 
-    def sign_out(self,) -> bool:
+    def sign_out(self, ) -> bool:
         pass
 
-    def sign_out_everywhere(self,) -> bool:
+    def sign_out_everywhere(self, ) -> bool:
         pass
 
     def sign_up(self, user: NewUser) -> str:
