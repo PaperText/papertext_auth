7b261ab33a6cbd0c3c850a5a21a6c27265941f03
Danil Kireev <danilunrandom@gmail.com>
2020-05-06 00:21:47 +0300

    fix: many typos, added necessary folder creation and added configs to container start script

--- start_db.py
+++ start_db.py
@@ -1,4 +1,4 @@
-#!/usr/cin/env python3
+#!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
 from os import getenv
@@ -6,24 +6,25 @@ from subprocess import call
 from typing import NoReturn
 
 
-def call_container_manafer(container_manager: str) -> NoReturn:
+def call_container_manager(container_manager: str) -> NoReturn:
     call(f"{container_manager} stop postgresql".split(" "))
     call(f"{container_manager} rm postgresql".split(" "))
-    call(f"{container_manager} run --name postgresql "
+    call(f"mkdir -p {getenv('HOME')}/.papertext/postgresql".split(" "))
+    call(f"{container_manager} run --name postgresql -d "
          "-e POSTGRES_PASSWORD=password "
+         "-e POSTGRES_DB=papertext "
          "-p 5432:5432 "
-         f"-v {getenv('HOME')}/.papertext/postgresql:/var/lib/postgresql/data "
-         "-d postgres".split(" "))
-    call(f"{container_manager} start postgresql".split(" "))
+         f"-v {getenv('HOME')}/.papertext/postgresql:/var/lib/postgresql/data:z "
+         "postgres".split(" "))
 
 
 def start_db():
     try:
         container_manager = "podman"
-        call_container_manafer(container_manager)
+        call_container_manager(container_manager)
     except FileNotFoundError:
         container_manager = "docker"
-        call_container_manafer(container_manager)
+        call_container_manager(container_manager)
 
 
 if __name__ == "__main__":
