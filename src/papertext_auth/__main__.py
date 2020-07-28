a9d043e0dcd4d8cee4dc901a6c09ae8d9d205f16
Danil Kireev <danilunrandom@gmail.com>
2020-07-28 20:08:49 +0300

    refactor: moved scripts to separate class

--- src/papertext_auth/__main__.py
+++ src/papertext_auth/__main__.py
@@ -11,18 +11,43 @@ pyproject_path = source_path / "pyproject.toml"
 pyproject_path = pyproject_path.resolve()
 
 
-def lint():
-    call(f"python -m flakehell lint {src_path}".split(" "))
+class Scripts:
+    @staticmethod
+    def lint_flakehell():
+        call(f"python -m flakehell lint {src_path}".split(" "))
 
+    @staticmethod
+    def lint_mypy():
+        call(f"python -m mypy {src_path}".split(" "))
 
-def fix_black():
-    call(f"python -m black {src_path} --config {pyproject_path}".split(" "))
+    @staticmethod
+    def lint():
+        Scripts.lint_flakehell()
+        Scripts.lint_mypy()
 
+    @staticmethod
+    def fix_black():
+        call(
+            f"python -m black {src_path} --config {pyproject_path}".split(" ")
+        )
 
-def fix_isort():
-    call(f"python -m isort -rc {src_path}".split(" "))
+    @staticmethod
+    def fix_isort():
+        call(f"python -m isort -rc {src_path}".split(" "))
 
+    @staticmethod
+    def fix():
+        Scripts.fix_black()
+        Scripts.fix_isort()
 
-def fix():
-    fix_black()
-    fix_isort()
+    @staticmethod
+    def docs_build():
+        call(
+            f"sphinx-build -b html {src_path / 'paperback_docs'} {source_path / 'docs'}".split(
+                " "
+            )
+        )
+
+    @staticmethod
+    def docs_clean():
+        call(f"rm -rf {source_path / 'docs'}".split(" "))
