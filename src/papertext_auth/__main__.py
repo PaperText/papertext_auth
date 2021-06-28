f6e303f23844fc2eb034820aaabe2d7a396ad53a
unrndm <unrndm@gmail.com>
2021-06-28 23:33:48 +0300

    chore(__main__): :art: reformated code and added some util functions

--- src/papertext_auth/__main__.py
+++ src/papertext_auth/__main__.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from subprocess import call
+from shlex import split
 
 src_path = Path(__file__) / ".." / ".."
 src_path = src_path.resolve()
@@ -7,39 +8,48 @@ src_path = src_path.resolve()
 source_path = src_path / ".."
 source_path = source_path.resolve()
 
+docs_path = source_path / "docs"
+docs_path = docs_path.resolve()
+
 pyproject_path = source_path / "pyproject.toml"
 pyproject_path = pyproject_path.resolve()
 
 
 class Scripts:
     @staticmethod
-    def lint_flakehell():
-        print("+--------------------+")
-        print("| [flake]hell linter |")
-        print("+--------------------+")
-        call(f"python -m flakehell lint {src_path}".split(" "))
+    def pretty_print(string):
+        print("+-"+"-"*len(string)+"-+")
+        print("| "+str(string)+" |")
+        print("+-" + "-" * len(string) + "-+")
+
+    @staticmethod
+    def execute(cmd):
+        return call(split(cmd))
+
+    @staticmethod
+    def lint_flake8():
+        Scripts.pretty_print("flake8[9] linter")
+        Scripts.execute(f"python -m flake8 {src_path}")
 
     @staticmethod
     def lint_mypy():
-        print("+-------------+")
-        print("| mypy linter |")
-        print("+-------------+")
-        call(f"python -m mypy {src_path}".split(" "))
+        Scripts.pretty_print("mypy linter")
+        Scripts.execute(f"python -m mypy {src_path}")
 
     @staticmethod
     def lint():
-        Scripts.lint_flakehell()
+        Scripts.lint_flake8()
         Scripts.lint_mypy()
 
     @staticmethod
     def fix_black():
-        call(
-            f"python -m black {src_path} --config {pyproject_path}".split(" ")
-        )
+        Scripts.pretty_print("black fixer")
+        Scripts.execute(f"python -m black {src_path} --config {pyproject_path}")
 
     @staticmethod
     def fix_isort():
-        call(f"python -m isort {src_path}".split(" "))
+        Scripts.pretty_print("isort fixer")
+        Scripts.execute(f"python -m isort {src_path}")
 
     @staticmethod
     def fix():
